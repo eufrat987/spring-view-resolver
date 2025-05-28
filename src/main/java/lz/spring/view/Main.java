ed70e172c7beabaa5ab54e01fab755d8cceca684
eufrat <lukzyw@gmail.com>
2025-05-28 18:44:07 +0200

    Example view resolver

---
+++ src/main/java/lz/spring/view/Main.java
@@ -0,0 +1,24 @@
+package lz.spring.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        var viewResolver = new UrlBasedViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(InternalResourceView.class);
+        return viewResolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/example.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Example Page</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+</body>
+</html>
